b712b5e31fab672df1aaee7cfc5139a64d94a5d4
matheus.cyntrao@gmail.com <matheus.cyntrao@gmail.com>
2019-08-03 13:37:50 -0300

    Adicionando mapeamento da página de customers.

--- src/main/java/PageObjects/CustomersPageObject.java
+++ src/main/java/PageObjects/CustomersPageObject.java
@@ -6,6 +6,15 @@ import org.openqa.selenium.support.FindBy;
 public class CustomersPageObject {
 
     @FindBy(css = "#gcrud-search-form > div.header-tools > div.floatL.t5 > a")
-    private WebElement buttonAddCustomer;
+    private WebElement button_addCustomer;
+
+    @FindBy(id = "switch-version-select")
+    private WebElement select_BootstrapVersion;
+
+    @FindBy(className = "customerName")
+    private WebElement textfield_customerName;
+
+    @FindBy(xpath = "//*[@id=\"gcrud-search-form\"]/div[2]/table/tbody/tr[1]/td[2]/div[1]/div/button")
+    private WebElement button_firstRegistryMoreOptions;
 
 }
